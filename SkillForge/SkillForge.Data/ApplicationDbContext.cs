140a854a1575864190c98538041bff5a81fe7f40
Niko Nikolov <nikonikolov2006@gmail.com>
2025-02-08 13:51:16 +0200

    Created the first entity and its migration: Courses

--- SkillForge/SkillForge.Data/ApplicationDbContext.cs
+++ SkillForge/SkillForge.Data/ApplicationDbContext.cs
@@ -1,9 +1,14 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SkillForge.Data.Entities;
 
 namespace SkillForge.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-    }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Courses> Courses { get; set; }
+    }  
 }
--- SkillForge/SkillForge.Data/Migrations/20250208114855_AddCoursesEntity.Designer.cs
+++ SkillForge/SkillForge.Data/Migrations/20250208114855_AddCoursesEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SkillForge.Data;
 
@@ -11,9 +12,11 @@ using SkillForge.Data;
 namespace SkillForge.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250208114855_AddCoursesEntity")]
+    partial class AddCoursesEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
